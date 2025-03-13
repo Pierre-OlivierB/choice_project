e9e0021bb765a055d93934312ba753a46e01a41d
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2025-03-13 07:56:38 +0100

    debug way chapter/event and verification action done and display none content alrready done

--- src/contents/book.jsx
+++ src/contents/book.jsx
@@ -16,12 +16,17 @@ function Book(props) {
   const [flag, setFlag] = useState(true);
   // get all stories from json
   const stories = storiesData;
+
+  // !------------------------------
+  const [choiceAlreadyDone, setChoiceAlreadyDone] = useState([]);
+  // !------------------------------
+
   // set actual storie
   const currentStories = stories[choice];
 
   // set precedent action
   const [precendentAction, setPrecendentAction] = useState(currentStories.txt);
-  const [actionSup, setActionSup] = useState([]);
+  // const [actionSup, setActionSup] = useState([]);
   const [actionActive, setActionActive] = useState(false);
 
   // save
@@ -40,7 +45,7 @@ function Book(props) {
     setChoice(newChapter);
     setFlag(!flag);
 
-    if (newChapter == 24) {
+    if (newChapter == 24 || newChapter == 26 || newChapter == 28) {
       setStoryEnd(true);
     }
   }
@@ -48,33 +53,34 @@ function Book(props) {
   // get data from child event
   function handleDiceFromEvent(child_choice) {
     // setDice(child_dice[0]);
+    console.log("TEST setflag " + child_choice[0]);
     setFlag(child_choice[0]);
 
     // event by the action win or not
     switch (child_choice[1]) {
       case "c_w":
         setPrecendentAction(currentStories.c_w[0]);
-        setActionSup(currentStories.c_w[1]);
+        // setActionSup(currentStories.c_w[1]);
         break;
       case "d_w":
         setPrecendentAction(currentStories.d_w[0]);
-        setActionSup(currentStories.d_w[1]);
+        // setActionSup(currentStories.d_w[1]);
         break;
       case "i_w":
         setPrecendentAction(currentStories.i_w[0]);
-        setActionSup(currentStories.i_w[1]);
+        // setActionSup(currentStories.i_w[1]);
         break;
       case "c_l":
         setPrecendentAction(currentStories.c_l[0]);
-        setActionSup(currentStories.c_l[1]);
+        // setActionSup(currentStories.c_l[1]);
         break;
       case "d_l":
         setPrecendentAction(currentStories.d_l[0]);
-        setActionSup(currentStories.d_l[1]);
+        // setActionSup(currentStories.d_l[1]);
         break;
       case "i_l":
         setPrecendentAction(currentStories.i_l[0]);
-        setActionSup(currentStories.i_l[1]);
+        // setActionSup(currentStories.i_l[1]);
         break;
 
       default:
@@ -92,48 +98,60 @@ function Book(props) {
   const [swipedCardIndex, setSwipedCardIndex] = useState(null);
   const handleSwipeRight = (index) => {
     setSwipedCardIndex(index);
+    console.log("swipe setflag " + flag);
   };
 
   useEffect(() => {
     let listBtn = [];
+
     for (let i = 0; i < btnList.length; i++) {
-      listBtn.push(
-        <ChoiceCard
-          key={i}
-          content={currentStories.btn_choix[i]}
-          onSwipeRight={() => {
-            handleSwipeRight(i);
-            const chc = currentStories.choix[i];
-            handleClick(chc);
-            setActionActive(false);
-            console.log("TEST book" + currentStories.choix[i]);
-          }}
-        />
-      );
+      // console.log(JSON.stringify(currentStories));
+      // console.log("what is in alreadydone ? " + choiceAlreadyDone);
+      if (!choiceAlreadyDone.includes(currentStories.choix[i])) {
+        // console.log("test num chap alreadydone" + currentStories.choix[i]);
+
+        listBtn.push(
+          <ChoiceCard
+            key={i}
+            content={currentStories.btn_choix[i]}
+            onSwipeRight={() => {
+              handleSwipeRight(i);
+              const chc = currentStories.choix[i];
+              handleClick(chc);
+              setActionActive(false);
+              setChoiceAlreadyDone([choiceAlreadyDone, choice]);
+              if (choice == 2) {
+                setChoiceAlreadyDone([choiceAlreadyDone, "6"]);
+              }
+              console.log("TEST book" + currentStories.choix[i]);
+            }}
+          />
+        );
+      }
     }
 
-    console.log("render 1", actionActive, actionSup);
+    console.log("render 1", actionActive);
     actionActive ? setPrecendentAction(currentStories.card_context) : null;
 
     // if action to add from precedent action, add button
 
-    if (actionSup && actionSup.length != 0) {
-      listBtn.push(
-        <ChoiceCard
-          key={5}
-          content={actionSup[1]}
-          onSwipeRight={() => {
-            const chc = actionSup[0];
-            setChoice(chc);
-            setActionSup([]);
-            setActionActive(true);
-          }}
-        />
-      );
-      console.log("test actionsup", actionSup);
-    }
+    // if (actionSup && actionSup.length != 0) {
+    //   listBtn.push(
+    //     <ChoiceCard
+    //       key={5}
+    //       content={actionSup[1]}
+    //       onSwipeRight={() => {
+    //         const chc = actionSup[0];
+    //         setChoice(chc);
+    //         setActionSup([]);
+    //         setActionActive(true);
+    //       }}
+    //     />
+    //   );
+    //   console.log("test actionsup", actionSup);
+    // }
     setBtnChoiceContent(listBtn);
-  }, [currentStories, actionSup, swipedCardIndex]);
+  }, [currentStories, swipedCardIndex, flag]);
 
   return !storyEnd ? (
     <div className="d-flex flex-column main-content">
