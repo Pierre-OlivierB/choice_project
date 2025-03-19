27d143523d794fec7e329f6c26f87374d7dec7ca
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2025-03-19 12:11:08 +0100

    add new content rules, del chapter num, add picto for character

--- src/contents/chapter/chapter.jsx
+++ src/contents/chapter/chapter.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserGraduate,
+  faUserNinja,
+  faUserSecret,
+  faExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Chapter(props) {
   const chpt = props.chpt;
@@ -20,19 +27,76 @@ function Chapter(props) {
     <div className="test">
       <div className="inside-content container-fluid d-flex flex-column justify-content-evenly">
         <div className="row justify-content-md-center">
-          <div className="col col-6">
+          <div className="col col-10">
             <div className="row">
-              <div className="col col-6">
-                <h1>chapitre {chpt}</h1>
+              <div className="col col-6 me-5">
+                <article className="p-2">
+                  <h1>Rappel des règles</h1>
+                  <section className="bg-light p-2 rounded">
+                    <p>Les règles sont les suivantes :</p>
+                    <div className="d-flex">
+                      <FontAwesomeIcon className="me-1" icon={faExclamation} />
+                      <p>
+                        Chaque action disponible correspond à une
+                        caractéristique.
+                      </p>
+                    </div>
+                    <div className="d-flex">
+                      <FontAwesomeIcon className="me-1" icon={faExclamation} />
+                      <p>
+                        L'issu du choix est lié à la probabilité de réussite
+                        correspondant à cette caractéristique.
+                      </p>
+                    </div>
+                    <div className="d-flex">
+                      <FontAwesomeIcon className="me-1" icon={faExclamation} />
+                      <p>
+                        Plus la caractéristique est grande, plus la réussite est
+                        probable.
+                      </p>
+                    </div>
+                  </section>
+                </article>
               </div>
-              <div className="col col-6">
+              <div className="col col-5">
                 <div className="card card-charac">
                   <div className="card-body">
-                    <h4 className="card-title">{perso.name}</h4>
-                    <h5 className="card-title">{perso.type}</h5>
-                    <p className="card-text">C : {perso.constitution}</p>
-                    <p className="card-text">D : {perso.dexterite}</p>
-                    <p className="card-text">I : {perso.intelligence}</p>
+                    <article className="d-flex justify-content-sm-evenly">
+                      <section>
+                        <h4 className="card-title">{perso.name}</h4>
+                        <h5 className="card-title">{perso.type}</h5>
+                        <p className="card-text">C : {perso.constitution}</p>
+                        <p className="card-text">D : {perso.dexterite}</p>
+                        <p className="card-text">I : {perso.intelligence}</p>
+                      </section>
+
+                      <section className="d-flex justify-content-md-center align-items-center w-50">
+                        {perso.intelligence == "75" ? (
+                          <FontAwesomeIcon
+                            className="w-50 h-50"
+                            icon={faUserGraduate}
+                          />
+                        ) : (
+                          <></>
+                        )}
+                        {perso.dexterite == "75" ? (
+                          <FontAwesomeIcon
+                            className="w-50 h-50"
+                            icon={faUserNinja}
+                          />
+                        ) : (
+                          <></>
+                        )}
+                        {perso.constitution == "75" ? (
+                          <FontAwesomeIcon
+                            className="w-50 h-50"
+                            icon={faUserSecret}
+                          />
+                        ) : (
+                          <></>
+                        )}
+                      </section>
+                    </article>
                   </div>
                   {inventory == "" ? (
                     <></>
@@ -45,7 +109,7 @@ function Chapter(props) {
           </div>
         </div>
         <div className="row justify-content-md-center">
-          <div className="col col-10">
+          <div className="col col-10 description p-4">
             <p>{txt}</p>
             <p>{mj}</p>
           </div>
--- src/contents/characters/characters.jsx
+++ src/contents/characters/characters.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import Book from "../book";
 import ChoiceCard from "../events/choicecard";
 import charactereData from "./data.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserGraduate,
+  faUserNinja,
+  faUserSecret,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Characters() {
   // get data from json of all characteres
@@ -24,6 +30,18 @@ function Characters() {
         <div className="test">
           <div className="main-content container-fluid d-flex justify-content-center">
             <div className="main-content row d-flex justify-content-evenly">
+              <div className="row justify-content-md-center mt-4">
+                <article className="col col-10 description p-4">
+                  <p>
+                    Dans un RP, il nous faut choisir un personnage qui va nou
+                    représenté. Pour l'exemple, voici 3 personnages
+                    pré-construit afin de vous familiariser avec la construction
+                    d'un personnage.
+                  </p>
+                  <p>Et maintenant à vous de jouer.</p>
+                  <p> Quel est votre choix ?</p>
+                </article>
+              </div>
               {data.map((tmp, key) => {
                 return (
                   <div
@@ -32,23 +50,50 @@ function Characters() {
                   >
                     <div className="card card-charac ">
                       <div className="card-body">
-                        <h4 className="card-title">Nom : {tmp.name}</h4>
-                        <h5 className="card-title">Fonction : {tmp.type}</h5>
-                        <p className="card-text">
-                          Constitution (C) : {tmp.constitution}
-                        </p>
-                        <p className="card-text">
-                          Dextérité (D) : {tmp.dexterite}
-                        </p>
-                        <p className="card-text">
-                          Intelligence (I) : {tmp.intelligence}
-                        </p>
-                        {/* <button
-                          className="btn btn-primary"
-                          onClick={(e) => handleClick(e)}
-                        >
-                          choix : {parseInt(tmp.choix) + 1}
-                        </button> */}
+                        <article className="d-flex justify-content-sm-evenly">
+                          <section>
+                            <h4 className="card-title">Nom : {tmp.name}</h4>
+                            <h5 className="card-title">
+                              Fonction : {tmp.type}
+                            </h5>
+                            <p className="card-text">
+                              Constitution (C) : {tmp.constitution}
+                            </p>
+                            <p className="card-text">
+                              Dextérité (D) : {tmp.dexterite}
+                            </p>
+                            <p className="card-text mb-5">
+                              Intelligence (I) : {tmp.intelligence}
+                            </p>
+                          </section>
+                          <section className="d-flex justify-content-md-center align-items-center w-50">
+                            {tmp.intelligence == "75" ? (
+                              <FontAwesomeIcon
+                                className="w-50 h-50"
+                                icon={faUserGraduate}
+                              />
+                            ) : (
+                              <></>
+                            )}
+                            {tmp.dexterite == "75" ? (
+                              <FontAwesomeIcon
+                                className="w-50 h-50"
+                                icon={faUserNinja}
+                              />
+                            ) : (
+                              <></>
+                            )}
+                            {tmp.constitution == "75" ? (
+                              <FontAwesomeIcon
+                                className="w-50 h-50"
+                                icon={faUserSecret}
+                              />
+                            ) : (
+                              <></>
+                            )}
+                          </section>
+                        </article>
+
                         <ChoiceCard
                           content={"choix : " + (parseInt(tmp.choix) + 1)}
                           onSwipeRight={() => handleClick(tmp.choix)}
